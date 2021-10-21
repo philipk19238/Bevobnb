38aa4a7f23b8c55c7a43eb114ed9ad5498562375
Philip Kung <pkung67@utexas.edu>
2021-10-20 23:16:42 -0500

    overriding old files

---
+++ BevobnbTeam30/BevobnbTeam30/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Globalization;
+
+namespace BevobnbTeam30
+
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.Use(async (context, next) =>
+            {
+                CultureInfo.CurrentCulture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
+                CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture;
+
+                await next.Invoke();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
